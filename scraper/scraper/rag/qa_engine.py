ea11711c5cd0b3b5068c936af8a216cdc0717b55
apariitmadras <24ds1000011@ds.study.iitm.ac.in>
2025-06-14 19:52:05 +0530

    Delete scraper/scraper directory

--- scraper/scraper/rag/qa_engine.py
+++
@@ -1,7 +0,0 @@
-# rag/qa_engine.py
-def get_answer(question, knowledge_base):
-    # Embed Q, search similar chunks, feed to LLM for answer
-    return {
-        "answer": "Pandas is a Python library for tabular data.",
-        "links": ["https://tds.s-anand.net/#/2025-01/"]
-    }
